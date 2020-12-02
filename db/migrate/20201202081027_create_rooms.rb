45591801d8ece9711cdcc286eb2ff49f49e4c184
WataruOjima <wataru.ojima20@gmail.com>
2020-12-02 17:15:36 +0900

    roomsテーブルの作成やり直し

--- db/migrate/20201202081027_create_rooms.rb
+++ db/migrate/20201202081027_create_rooms.rb
@@ -1,9 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string ;name null: false
+      t.string :name, null: false
       t.timestamps
-    
     end
   end
 end
